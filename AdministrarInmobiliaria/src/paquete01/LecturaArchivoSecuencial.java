35d16cf2a545dc1bc193f89506e70379e2fa54f6
MAteoVo <mateoavalarezo@gmail.com>
2022-05-28 18:35:46 -0500

    Vala

---
+++ AdministrarInmobiliaria/src/paquete01/LecturaArchivoSecuencial.java
@@ -0,0 +1,379 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete01;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import java.util.ArrayList;
+import paquete02.Propietario;
+import paquete03.Barrio;
+import paquete04.Ciudad;
+import paquete05.Constructora;
+import paquete06.Casa;
+import paquete06.Departamento;
+/**
+ *
+ * @author mateovalarezoojeda
+ */
+public class LecturaArchivoSecuencial {
+    private ObjectInputStream entrada;
+    private ArrayList<Casa> casas;
+    private ArrayList<Departamento> departamentos;
+    private ArrayList<Propietario> propietarios;
+    private ArrayList<Barrio> barrios;
+    private ArrayList<Ciudad> ciudades;
+    private ArrayList<Constructora> constructoras;
+    private String nombreArchivo;
+    private String identificador;
+    private Propietario propietarioBuscado;
+    private Ciudad ciudadBuscado;
+    private Barrio barrioBuscado;
+    private Constructora constructoraBuscado;
+    
+    public LecturaArchivoSecuencial(String n) {
+        nombreArchivo = n;
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+            try // abre el archivo
+            {
+                entrada = new ObjectInputStream(
+                        new FileInputStream(n));
+            } // fin de try
+            catch (IOException ioException) {
+                System.err.println("Error al abrir el archivo.");
+
+            } // fin de catch
+        }
+    }
+    public void establecerNombreArchivo(String n) {
+        nombreArchivo = n;
+    }
+    public void establecerListaCasas() {
+        // 
+        casas = new ArrayList<>();
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Casa registro = (Casa) entrada.readObject();
+                    casas.add(registro);
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    // System.err.println("No hay datos en el archivo: " + ex);
+                    break;
+                }
+            }
+        }
+
+    }
+    public void establecerListaDepartamentos() {
+        // 
+        departamentos = new ArrayList<>();
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Departamento registro = (Departamento) entrada.readObject();
+                    departamentos.add(registro);
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    // System.err.println("No hay datos en el archivo: " + ex);
+                    break;
+                }
+            }
+        }
+
+    }
+    public void establecerListaPropietarios() {
+        // 
+        departamentos = new ArrayList<>();
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Propietario registro = (Propietario) entrada.readObject();
+                    propietarios.add(registro);
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    // System.err.println("No hay datos en el archivo: " + ex);
+                    break;
+                }
+            }
+        }
+
+    }
+    public void establecerListaBarrios() {
+        // 
+        departamentos = new ArrayList<>();
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Barrio registro = (Barrio) entrada.readObject();
+                    barrios.add(registro);
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    // System.err.println("No hay datos en el archivo: " + ex);
+                    break;
+                }
+            }
+        }
+
+    }
+    public void establecerListaCiudades() {
+        // 
+        departamentos = new ArrayList<>();
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Ciudad registro = (Ciudad) entrada.readObject();
+                    ciudades.add(registro);
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    // System.err.println("No hay datos en el archivo: " + ex);
+                    break;
+                }
+            }
+        }
+
+    }
+    public void establecerListaConstructoras() {
+        // 
+        departamentos = new ArrayList<>();
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Constructora registro = (Constructora) entrada.readObject();
+                    constructoras.add(registro);
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    // System.err.println("No hay datos en el archivo: " + ex);
+                    break;
+                }
+            }
+        }
+
+    }
+    public ArrayList<Casa> obtenerListaCasas() {
+        return casas;
+    }
+    
+    public ArrayList<Departamento> obtenerListaDepartamento() {
+        return departamentos;
+    }
+    public ArrayList<Propietario> obtenerListaPropietarios() {
+        return propietarios;
+    }
+    
+    public ArrayList<Barrio> obtenerListaBarrios() {
+        return barrios;
+    }
+    public ArrayList<Ciudad> obtenerListaCiudades() {
+        return ciudades;
+    }
+    
+    public ArrayList<Constructora> obtenerListaConstructoras() {
+        return constructoras;
+    }
+    public String obtenerNombreArchivo() {
+        return nombreArchivo;
+    }
+    
+    public void cerrarArchivo() {
+        try // cierra el archivo y sale
+        {
+            if (entrada != null) {
+                entrada.close();
+            }
+            System.exit(0);
+        } // fin de try
+        catch (IOException ioException) {
+            System.err.println("Error al cerrar el archivo.");
+            System.exit(1);
+        } // fin de catch
+    } // 
+    public void establecerIdentificador(String n) {
+        identificador = n;
+    }
+    public void establecerPropietarioBuscado() {
+        // 
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Propietario registro = (Propietario) entrada.readObject();
+                    if(registro.obtenerIdentificacion().equals(identificador)){
+                        propietarioBuscado = registro;
+                        break;
+                    }
+                    
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+                    // se puede usar el break;
+                    // System.err.println("Fin de archivo: " + endOfFileException);
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    System.err.println("No hay datos en el archivo: " + ex);
+
+                }
+            }
+        }
+    }
+    public void establecerBarrioBuscado() {
+        // 
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Barrio registro = (Barrio) entrada.readObject();
+                    if(registro.obtenerNombreBarrio().equals(identificador)){
+                        barrioBuscado = registro;
+                        break;
+                    }
+                    
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+                    // se puede usar el break;
+                    // System.err.println("Fin de archivo: " + endOfFileException);
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    System.err.println("No hay datos en el archivo: " + ex);
+
+                }
+            }
+        }
+    }
+    public void establecerCiudadBuscado() {
+        // 
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Ciudad registro = (Ciudad) entrada.readObject();
+                    if(registro.obtenerNombreCuidad().equals(identificador)){
+                        ciudadBuscado = registro;
+                        break;
+                    }
+                    
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+                    // se puede usar el break;
+                    // System.err.println("Fin de archivo: " + endOfFileException);
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    System.err.println("No hay datos en el archivo: " + ex);
+
+                }
+            }
+        }
+    }
+    public void establecerConstructoraBuscado() {
+        // 
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Constructora registro = (Constructora) entrada.readObject();
+                    if(registro.obtenerIdEmpresa().equals(identificador)){
+                        constructoraBuscado = registro;
+                        break;
+                    }
+                    
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+                    // se puede usar el break;
+                    // System.err.println("Fin de archivo: " + endOfFileException);
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    System.err.println("No hay datos en el archivo: " + ex);
+
+                }
+            }
+        }
+    }
+    public String obtenerIdentificador() {
+        return identificador;
+    }
+        
+    public Propietario obtenerPropietarioBuscado() {
+        return propietarioBuscado;
+    }
+    public Barrio obtenerBarrioBuscado() {
+        return barrioBuscado;
+    }
+    public Ciudad obtenerCiudadBuscado() {
+        return ciudadBuscado;
+    }
+    public Constructora obtenerConstructoraBuscado() {
+        return constructoraBuscado;
+    } 
+}
