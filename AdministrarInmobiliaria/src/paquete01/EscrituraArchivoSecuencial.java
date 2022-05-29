0adf612f74303a622d124546bb7e1a244d027c8e
TheLosser01 <victormvega03@gmail.com>
2022-05-29 00:38:15 -0500

    casi me sale

--- AdministrarInmobiliaria/src/paquete01/EscrituraArchivoSecuencial.java
+++ AdministrarInmobiliaria/src/paquete01/EscrituraArchivoSecuencial.java
@@ -5,6 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import paquete02.Propietario;
+import paquete03.Barrio;
+import paquete04.Ciudad;
+import paquete05.Constructora;
 import paquete06.Departamento;
 import paquete06.Casa;
 
@@ -14,10 +18,14 @@ public final class EscrituraArchivoSecuencial {
     private Departamento registro;
     private ArrayList<Departamento> listaDepartamentos;
     private ArrayList<Casa> listaCasas;
+    private ArrayList<Propietario> listaPropietarios;
+    private ArrayList<Barrio> listaBarrios;
+    private ArrayList<Ciudad> listaCiudades;
+    private ArrayList<Constructora> listaConstructoras;
 
     public EscrituraArchivoSecuencial(String nombreArc) {
         nombreArchivo = nombreArc;
-        establecerLista(); // obtener los valores (objetos)
+        establecerListaDepartamentos(); // obtener los valores (objetos)
                                     // que tiene el archivo.
         // System.out.println(obtenerListaProfesores().size());
         try // abre el archivo
@@ -25,11 +33,29 @@ public final class EscrituraArchivoSecuencial {
             salida = new ObjectOutputStream(
                     new FileOutputStream(nombreArchivo));
             // proceso para ingresar nuevamente los valores del archivo
-            if (obtenerLista().size() > 0) {
-                for (int i = 0; i < obtenerLista().size(); i++) {
-                    establecerRegistro(obtenerLista().get(i));
+            if (obtenerListaDepartamentos().size() > 0) {
+                for (int i = 0; i < obtenerListaDepartamentos().size(); i++) {
+                    establecerRegistro(obtenerListaDepartamentos().get(i));
                     establecerSalida();
-                }
+                }            
+            }
+        } // fin de try
+        catch (IOException ioException) {
+            System.err.println("Error al abrir el archivo.");
+        } // fin de catch
+        establecerListaCasas(); // obtener los valores (objetos)
+                                    // que tiene el archivo.
+        // System.out.println(obtenerListaProfesores().size());
+        try // abre el archivo
+        {
+            salida = new ObjectOutputStream(
+                    new FileOutputStream(nombreArchivo));
+            // proceso para ingresar nuevamente los valores del archivo
+            if (obtenerListaCasas().size() > 0) {
+                for (int i = 0; i < obtenerListaCasas().size(); i++) {
+                    establecerRegistro(obtenerListaCasas().get(i));
+                    establecerSalida();
+                }            
             }
         } // fin de try
         catch (IOException ioException) {
@@ -55,10 +81,40 @@ public final class EscrituraArchivoSecuencial {
 
     // en el atributo listaProfesores obtenemos los registros 
     // del archivo
-    public void establecerLista() {
+    public void establecerListaDepartamentos() {
         LecturaArchivoSecuencial l = new LecturaArchivoSecuencial(obtenerNombreArchivo());
         l.establecerListaDepartamentos();
         listaDepartamentos = l.obtenerListaDepartamento();
+    }
+    
+    public void establecerListaCasas() {
+        LecturaArchivoSecuencial l = new LecturaArchivoSecuencial(obtenerNombreArchivo());
+        l.establecerListaCasas();
+        listaCasas = l.obtenerListaCasas();
+    }
+    
+    public void establecerListaPropietarios() {
+        LecturaArchivoSecuencial l = new LecturaArchivoSecuencial(obtenerNombreArchivo());
+        l.establecerListaPropietarios();
+        listaPropietarios = l.obtenerListaPropietarios();
+    }
+    
+    public void establecerListaBarrios() {
+        LecturaArchivoSecuencial l = new LecturaArchivoSecuencial(obtenerNombreArchivo());
+        l.establecerListaBarrios();
+        listaBarrios = l.obtenerListaBarrios();
+    }
+    
+    public void establecerListaCiudades() {
+        LecturaArchivoSecuencial l = new LecturaArchivoSecuencial(obtenerNombreArchivo());
+        l.establecerListaCiudades();
+        listaCiudades = l.obtenerListaCiudades();
+    }
+    
+    public void establecerListaConstructora() {
+        LecturaArchivoSecuencial l = new LecturaArchivoSecuencial(obtenerNombreArchivo());
+        l.establecerListaConstructoras();
+        listaConstructoras = l.obtenerListaConstructoras();
     }
 
     public String obtenerNombreArchivo(){
@@ -67,6 +123,26 @@ public final class EscrituraArchivoSecuencial {
     
     public ArrayList<Departamento> obtenerListaDepartamentos() {
         return listaDepartamentos;
+    }
+    
+    public ArrayList<Casa> obtenerListaCasas() {
+        return listaCasas;
+    }
+    
+    public ArrayList<Propietario> obtenerListaPropietarios() {
+        return listaPropietarios;
+    }
+    
+    public ArrayList<Barrio> obtenerListaBarrios() {
+        return listaBarrios;
+    }
+    
+    public ArrayList<Ciudad> obtenerListaCiudad() {
+        return listaCiudades;
+    }
+    
+    public ArrayList<Constructora> obtenerListaConstructora() {
+        return listaConstructoras;
     }
 
     public ObjectOutputStream obtenerSalida(){
